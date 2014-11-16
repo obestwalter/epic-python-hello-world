0b0732a9318d2d7de5d728d7894bd781e91f7076
Oliver Bestwalter <oliver@bestwalter.de>
2014-11-16 20:35:35 +0100

    md -> rst for later

--- tools/md2rst.py
+++ tools/md2rst.py
@@ -1,1 +1,12 @@
-from plumbum
+import subprocess
+from plumbum.path import LocalWorkdir, LocalPath
+
+if __name__ == '__main__':
+    for p in LocalWorkdir().up().walk():
+        assert isinstance(p, LocalPath)
+        if p.basename.endswith('.md'):
+            cmd = (["pandoc", "--from=markdown", "--to=rst", "-o",
+                    "%s" % (p.dirname / (p.basename[:-3] + '.rst')),
+                    "%s" % (p)])
+            print(" ".join(cmd))
+            subprocess.check_output(cmd)
